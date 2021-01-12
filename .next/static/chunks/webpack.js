141b1b20ff6e434185d1baf70a6200b4740f1180
NardaC <nardac6@gmail.com>
2021-01-12 09:58:54 -0500

    header nuevo

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "86f901e64ae0382fc530";
+/******/ 	var hotCurrentHash = "8a3e55fea4df99b1cf7a";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
