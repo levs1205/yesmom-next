32ad6b4a4c8737a995e21feacd946e6e79e18732
NardaC <nardac6@gmail.com>
2021-03-06 00:42:05 -0500

    sort fecha

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -8,6 +8,17 @@ import CardBlog from "../../components/CardBlog";
 import Head from "next/head";
 
 const Blog = ({ currentData }) => {
+
+console.log(currentData);
+
+  const sortFunction=(a, b)=> {
+    var dateA = new Date(a.blog.fecha).getTime();
+    console.log(dateA);
+    var dateB = new Date(b.blog.fecha).getTime();
+    return dateA < dateB ? 1 : -1;
+  }
+  currentData.sort(sortFunction)
+
   return (
     <AppLayout>
       <Head>
