992c7f8a14a56c8e8f4b6a0d98ee6230ed9f2c0a
NardaC <nardac6@gmail.com>
2021-03-16 15:09:30 -0500

    cambio de num

--- components/WsBtn.js
+++ components/WsBtn.js
@@ -4,7 +4,7 @@ import Image from "next/image";
 const WsBtn = () => {
   return (
     <>
-      <a target="blank" href="https://api.whatsapp.com/send?phone=+51954474402&text=Hola!%20Bienvenid@%20a%20Yes%20Mom.%20Estamos%20muy%20felices%20de%20que%20seas%20parte%20de%20nuestra%20comunidad.%20Te%20contestaremos%20antes%20de%20las%2024%20hrs.%20Mientras%20tanto,%20puedes%20revisar%20el%20blog%20en%20nuestra%20web.%20Hemos%20puesto%20mucho%20esfuerzo%20para%20que%20mam%C3%A1s%20reales%20creen%20contenido%20importante%20para%20ti.%20%20En%20caso%20est%C3%A9s%20interesado%20en%20vender%20en%20Yes%20Mom,%20puedes%20escribirnos%20a%20info@yesmom.com.pe%20%20Gracias,%20Yes%20Mom%20Team">
+      <a target="blank" href="https://api.whatsapp.com/send?phone=+51947234452&text=Hola!%20Bienvenid@%20a%20Yes%20Mom.%20Estamos%20muy%20felices%20de%20que%20seas%20parte%20de%20nuestra%20comunidad.%20Te%20contestaremos%20antes%20de%20las%2024%20hrs.%20Mientras%20tanto,%20puedes%20revisar%20el%20blog%20en%20nuestra%20web.%20Hemos%20puesto%20mucho%20esfuerzo%20para%20que%20mam%C3%A1s%20reales%20creen%20contenido%20importante%20para%20ti.%20%20En%20caso%20est%C3%A9s%20interesado%20en%20vender%20en%20Yes%20Mom,%20puedes%20escribirnos%20a%20info@yesmom.com.pe%20%20Gracias,%20Yes%20Mom%20Team">
         <div className="btn-ws">
           <Image
             src="/image/ws-yesmom.png"
