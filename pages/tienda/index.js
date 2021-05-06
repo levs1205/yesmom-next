dc423355aebf3c86586bc8e29167a1f4ecf16b94
Napster <rome.1205h@gmail.com>
2021-05-06 13:44:59 -0500

    estructuracion de la tienda

--- pages/tienda/index.js
+++ pages/tienda/index.js
@@ -3,13 +3,27 @@ import AppLayout from '../../components/AppLayout'
 import Image from "next/image";
 import Head from "next/head";
 import axios from 'axios';
-import { CardDeck, Container } from "react-bootstrap";
+import { CardDeck, Carousel, Col, Container, Row } from "react-bootstrap";
 import CardProduct from "../../components/CardProduct";
+import SidebarProducto from "./SidebarProducto";
 
 const Product = () => {
 
-    const [respuesta, setRespuesta] = useState([]);
+    const initialState = [
+        {
+            id: 1,
+            image: '/image/tienda/banner1.svg'
+        },
+        {
+            id: 2,
+            image: '/image/tienda/banner1.svg'
+        }
+    ]
 
+    const [banner, setBanner] = useState(initialState)
+
+
+    const [respuesta, setRespuesta] = useState([]);
     useEffect(async () => {
         const consulta = await axios('https://fakestoreapi.com/products');
         setRespuesta(consulta.data);
@@ -59,20 +73,37 @@ const Product = () => {
                 <meta name="twitter:creator" content="@JudithCristinaQ" />
             </Head>
             <div className="box-producto">
-                <h4 className="text-title-tienda">
-                    Lo + vendido
-                </h4>
-                <div className="box-product-general">
-                    <div className="box-card-group">
-                        <Container>
-                            <CardDeck style={{justifyContent:"center"}}>
-                                {respuesta.map(cardProduct => (
-                                    <CardProduct product={cardProduct} key={cardProduct.id}/>
-                                ))}
-                            </CardDeck>
-                        </Container>
-                    </div>
-                </div>
+                <Carousel className="box-carousel">
+                    {banner.map(ban => (
+                        <Carousel.Item className="carousel-item">
+                            <img src={ban.image} alt=""/>
+                            {/* <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/fa/Siula_Grande_072530032013.jpg/1200px-Siula_Grande_072530032013.jpg" alt=""/> */}
+                        </Carousel.Item>
+                    ))}
+                </Carousel>
+                <Container>
+                    <Row>
+                        <Col sm={3}>
+                            <SidebarProducto/>
+                        </Col>
+                        <Col sm={9}>
+                            <h4 className="text-title-tienda">
+                                Lo + vendido
+                            </h4>
+                            <div className="box-product-general">
+                                <div className="box-card-group">
+                                    <Container>
+                                        <CardDeck style={{justifyContent:"center"}}>
+                                            {respuesta.map(cardProduct => (
+                                                <CardProduct product={cardProduct} key={cardProduct.id}/>
+                                            ))}
+                                        </CardDeck>
+                                    </Container>
+                                </div>
+                            </div>
+                        </Col>
+                    </Row>
+                </Container>
             </div>
             <style jsx>
                 {`
@@ -81,8 +112,7 @@ const Product = () => {
                     display: flex;
                     justify-content: center;
                     flex-direction: column;
-                    padding: 3rem 9rem;
-                    font-family: "mont-regular";
+                    padding: 3rem 0rem;
                 }
                 .box-producto {
                     background-size: cover;
@@ -108,15 +138,16 @@ const Product = () => {
                     top: 0rem;
                     z-index: 10;
                 }
-                @media (min-width: 1800px) {
+                @media (min-width: 1024px) {
                     .box-product-general {
                         padding: 2rem 0rem;
                     }
-                    .text-title-tienda {
-                        margin: 1.5rem 0.5rem 0rem;
+                    .carousel-item{
+                        max-height: 34rem !important;
+                        margin-top: 0px !important;
                     }
                 }
-                @media (max-width: 768px) {
+                @media (max-width: 480px) {
                     .box-product-general {
                         width: 100%;
                         display: flex;
@@ -129,10 +160,16 @@ const Product = () => {
                         top: 0rem;
                         z-index: 10;
                     }
-                    .text-title-tienda {
-                        margin: 1.5rem 0.5rem 0rem;
+                    .carousel-item{
+                        max-height: 32rem !important;
+                        margin-top: 0px !important;
+                    }
+                    .carousel-item img{
+                        max-height: 32rem !important;
+                        margin-top: 0px !important;
                     }
                 }
+
                 `}
             </style>
         </AppLayout>
