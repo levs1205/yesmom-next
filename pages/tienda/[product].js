b999229f88706f397cf7c1f674c2814858a7bdad
Judith Cristina Quiñones Inga <judicriss18@gmail.com>
2021-05-24 13:18:04 -0500

    F2 descripción de producto específico

--- pages/tienda/[product].js
+++ pages/tienda/[product].js
@@ -34,34 +34,64 @@ const Product = () => {
             tenetur error doloribus harum reprehenderit porro ut, sapiente
           </p>
           <p className='price-text'>S/ 68.00</p>
-          <div>
-            <select name="" id="">
-              <option value="">sdssd</option>
-              <option value="">sdsdsd</option>
-              <option value="">sds</option>
-            </select>
-            <select name="" id="">
-              <option value="">sdssd</option>
-              <option value="">sdsdsd</option>
-              <option value="">sds</option>
-            </select>
+          <div className='box--select'>
+            <div>
+              <p>Color</p>
+              <select name="" id="" className="style-select">
+                <option value="">sdssd</option>
+                <option value="">sdsdsd</option>
+                <option value="">sds</option>
+              </select>
+            </div>
+            <div>
+              <p>Talla</p>
+              <select name="" id="" className="style-select">
+                <option value="">sdssd</option>
+                <option value="">sdsdsd</option>
+                <option value="">sds</option>
+              </select>
+            </div>
           </div>
-          <div>
+          <div className='box-quantity'>
             <p>Cantidad</p>
+            <div className='box-input-quantity'>
+              <input type="button" className="btn--subtract"/>
+              <input type="number" className="btn--number"/>
+              <input type="button" className="btn--plus"/>
+            </div>
           </div>
-          <div>
-            <button>Agregar al Carrito</button>
-            <button>Ver la tienda</button>
+          <div className='box-btn'>
+            <button className="btn-cart">Agregar al Carrito</button>
+            <button className="btn-store">Ver la tienda</button>
           </div>
         </section>
       </div>
+      <section className='container--product_description'>
+        <h6>Detalle del Producto</h6>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. A tortor, diam molestie et rhoncus, amet lacus, velit. Ac ipsum fames gravida habitant aliquet orci. Imperdiet egestas morbi egestas posuere diam. Pharetra, sit volutpat varius sed sit urna euismod. Viverra nunc turpis nulla at et venenatis vitae, facilisis fringilla. Quam aliquet et proin nulla lacus aliquet quam</p>
+        <h6>Puntos destacados del producto</h6>
+        <ul>
+          <li>Haretra, sit volutpat varius</li>
+          <li>Sed sit urna euismod - Viverra nunc turpis </li>
+          <li>Quam aliquet et </li>
+          <li>Proin nulla lacus quam</li>
+        </ul>
+        <h6>País de producción</h6>
+        <p>Pharetra, sit volutpat variusam</p>
+        <h6>Dimenciones</h6>
+        <p>xx-xx cm</p>
+        <h6>Material de producto</h6>
+        <p>Pharetra, sit volutpat varius</p>
+        <h6>Terminos y condiciones</h6>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. A tortor, diam molestie et rhoncus, amet lacus, velit. Ac ipsum fames gravida habitant aliquet orci. Imperdiet </p>
+      </section>
       <style jsx>
         {`
           .container--firstProductBlock {
             width: 100%;
             display: flex;
             flex-direction: column;
-            padding: 6rem 1rem 3rem 1rem;
+            padding: 6rem 1rem 1rem 1rem;
           }
           .firstProductBlock--carousel {
             width: 100%;
@@ -78,13 +108,13 @@ const Product = () => {
             color: #5A5A5A;
           }
         .firstProductBlock--details .sub-title{
-          font-family: "mont-light"!important;
+           font-family: "mont-regular"!important;
           font-style: normal;
           font-size:1rem;
           color: #5A5A5A;
         }
          .firstProductBlock--details .short-description{
-          font-family: "mont-light"!important;
+          font-family: "mont-regular"!important;
           font-style: normal;
           font-size:0.7rem;
           color: #5A5A5A;
@@ -113,6 +143,164 @@ const Product = () => {
         :global(.carousel .control-dots){
           display:none
         }
+        .style-select {
+          display: block;
+          font-size: 20px;
+           font-family: "mont-regular"!important;
+          color: #556EA1;
+          line-height: 1.3;
+          padding: .4em 1.4em .3em .8em;
+          width: 9rem;
+          box-sizing: border-box;
+          border:1px solid #556EA1;
+          box-shadow: 0 1px 0 1px rgba(0,0,0,.03);
+          border-radius: 15px;
+          -moz-appearance: none;
+          -webkit-appearance: none;
+          appearance: none;
+          background-color: #fff;
+          background-image: url('https://www.pinclipart.com/picdir/middle/186-1864292_select-the-law-down-to-the-single-arrow.png'),
+            linear-gradient(to bottom, #ffffff 0%,#f7f7f7 100%);
+          background-repeat: no-repeat, repeat;
+          background-position: right .7em top 50%, 0 0;
+          background-size: .65em auto, 100%;
+           outline:none;
+           margin-right:  0.5rem
+        }
+        .box--select div p{
+           font-family: "mont-regular";
+        }
+        .box-quantity  {
+          margin-top:1rem
+        }
+        .box-quantity p {
+           font-family: "mont-regular";
+        }
+        .btn--plus{
+          background-image:url('/image/icon/plus.svg');
+          background-color:#FFFFFF;
+          background-repeat:no-repeat;
+          background-position:center;
+          background-size: 10px 10px;
+          padding:10px 15px;
+          border: 1px solid #556EA1;
+          box-sizing: border-box;
+          border-radius: 4px;
+          height:42px;
+          width:42px
+        }
+
+         .btn--subtract{
+          background-image:url('/image/icon/subtract.svg');
+          background-color:#FFFFFF;
+          background-repeat:no-repeat;
+          background-position:center;
+          background-size: 10px 10px;
+          padding:auto 15px;
+          border: 1px solid #556EA1;
+          box-sizing: border-box;
+          border-radius: 4px;
+           height:42px;
+          width:42px
+        }
+        .btn--number{
+          height:42px;
+          width:42px;
+          border: 1px solid #556EA1;
+          box-sizing: border-box;
+          border-radius: 4px;
+          outline:none;
+           text-align: center;
+          font-family: "mont-light";
+          font-style: normal;
+          font-weight: normal;
+          font-size: 20px;
+          line-height: 36px;
+          /* identical to box height, or 180% */
+
+          display: flex;
+          align-items: center;
+
+          color: #5A5A5A;
+        }
+        .box-input-quantity{
+          display:flex;
+          justify-content:left;
+          align-items:center
+        }
+         .btn-store{
+          padding: 1.5rem;
+          text-align: center;
+          font-family: "omnes-regular";
+          font-weight: 500;
+          font-size: 1.3rem;
+          width: 310px;
+          height: 60px;
+          margin: auto;
+          background-color: #febf41;
+          border-radius: 20px;
+          color: #fff !important;
+          line-height: 1.3rem;
+          position: relative;
+          top: -3%;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+           border: 1px solid #febf41;
+          cursor: pointer;
+        }
+        .btn-cart{
+          padding: 1.5rem;
+          text-align: center;
+          font-family: "omnes-regular";
+          font-weight: 500;
+          font-size: 1.3rem;
+          width: 310px;
+          height: 60px;
+          margin: auto;
+          background-color:  #EC608D;
+          border-radius: 20px;
+          color: #fff !important;
+          line-height: 1.3rem;
+          position: relative;
+          top: -3%;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+           border: 1px solid  #EC608D;
+          cursor: pointer;
+        }
+        .box--select{
+          display:flex;
+          flex-direction:row;
+        }
+        .box-btn{
+          display:flex;
+          justify-content:center;
+          align-items:center;
+          flex-direction:column;
+          margin : 2rem 0rem
+        }
+         .box-btn button{
+          margin : 1rem 0.5rem
+        }
+        .container--product_description{
+          padding:0rem 2rem;
+          font-family: "mont-regular";
+          font-size:0.8rem;
+          font-weight: 300;
+          line-height: 35px;
+          color:#5A5A5A
+        }
+        .container--product_description p{
+          line-height: 35px;
+        }
+         .container--product_description h6{
+          font-family: "mont-semibold";
+          font-size:1rem;
+          font-weight: 600;
+          line-height: 36px;
+        }
         @media (min-width: 768px) {
            .container--firstProductBlock {
             width: 100%;
@@ -124,6 +312,9 @@ const Product = () => {
             width: 50%;
           }
           .firstProductBlock--details {
+            display:flex;
+            justify-content:center;
+            flex-direction:column;
             width: 50%;
             padding: 0rem 2rem;
           }
@@ -138,6 +329,40 @@ const Product = () => {
           }
            .firstProductBlock--details .price-text{
             font-size:1.5rem;
+          }
+          .box--select{
+            display:flex
+          }
+          .box-btn{
+            display:flex;
+            justify-content:center;
+            align-items:center;
+            flex-direction:row;
+            margin : 2rem 0rem
+          }
+          .style-select {
+            width: 250px;
+
+          }
+          .btn-store, .btn-cart{
+            font-family: "omnes-bold";
+            font-weight: 700;
+            width: 300px;
+            height: 60px;
+          }
+
+           .container--product_description{
+            padding:0rem 8rem;
+            font-family: "mont-regular";
+            font-size:1rem;
+            line-height:25px;
+             font-weight: 500;
+          }
+           .container--product_description h6{
+            font-family: "mont-semibold";
+            font-size:1.2rem;
+             line-height:36px;
+            font-weight: 800;
           }
         }
         `}
