9efb0cf9090caf645770761291946b5c331ae16c
Judith Cristina Quiñones Inga <judicriss18@gmail.com>
2021-05-25 13:59:20 -0500

    page shopping-cart

---
+++ pages/shopping-cart/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import AppLayout from "../../components/AppLayout";
+
+const shoppingCart = () => {
+  return (
+    <AppLayout>
+      <div>
+        <p>Carrito de compras</p>
+      </div>
+    </AppLayout>
+  )
+}
+
+export default shoppingCart
