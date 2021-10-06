4aa5a88329c3f9fe2efd4374260814dc0675c205
NardaC <nardac6@gmail.com>
2021-10-06 12:10:02 -0500

    cmabios

--- components/AppLayout/index.js
+++ components/AppLayout/index.js
@@ -10,17 +10,21 @@ const appLayout = ({ children }) => {
     <div className="box-yesmom">
       <Head>
         <title>yesmom</title>
+        {/* <link
+          rel="stylesheet"
+          href="https://stackpath.bootstrapcdn.com/bootswatch/4.5.2/flatly/bootstrap.min.css"
+        /> */}
         <meta name="description" content="esto es descripcion"></meta>
       </Head>
+      {/* <Header></Header> */}
       <HeaderNuevo />
-      <main className="main-yesmom">{children}</main>
-      <WsBtn />
-
+      <main>{children}</main>
+      <WsBtn/>
       <Footer></Footer>
       <style jsx global>
         {`
           html {
-            font-size: 62.5%;
+            font-size: 0.98vw !important;
             font-family: "mont-heavy";
           }
           body {
@@ -80,56 +84,6 @@ const appLayout = ({ children }) => {
             src: url(/fonts/Mont-Light.otf);
           }
 
-          @font-face {
-            font-family: "mont-bold";
-            src: url(/fonts/Mont-bold.ttf);
-          }
-          
-          @keyframes animateHeart {
-            0% {
-              transform: scale(1);
-            }
-            40% {
-              transform: scale(1.09);
-            }
-            100% {
-              transform: scale(1.1);
-            }
-          }
-
-          .fade-in {
-            -webkit-animation: fade-in 1.2s cubic-bezier(0.39, 0.575, 0.565, 1)
-              both;
-            animation: fade-in 1.2s cubic-bezier(0.39, 0.575, 0.565, 1) both;
-          }
-          @-webkit-keyframes fade-in {
-            0% {
-              opacity: 0;
-            }
-            100% {
-              opacity: 1;
-            }
-          }
-          @keyframes fade-in {
-            0% {
-              opacity: 0;
-            }
-            100% {
-              opacity: 1;
-            }
-          }
-
-          .animated {
-            -webkit-animation-duration: 3s;
-            animation-duration: 3s;
-            -webkit-animation-fill-mode: both;
-            animation-fill-mode: both;
-          }
-
-          .main-yesmom {
-            overflow: hidden;
-            padding-top: 3rem;
-          }
           .mt-views {
             padding-top: 0rem;
           }
@@ -145,13 +99,6 @@ const appLayout = ({ children }) => {
             margin-left: 0px !important;
             margin-right: 0px !important;
           }
-          .sin-margin-flex {
-            margin-left: 0px !important;
-            margin-right: 0px !important;
-            display: flex;
-            align-items: center;
-            justify-content: center;
-          }
           .pl-10 {
             padding-left: 6rem;
           }
@@ -166,26 +113,10 @@ const appLayout = ({ children }) => {
           .cl-fuxia {
             color: #ec608d;
           }
-          .cl-yellow {
-            color: #febf41;
-          }
-
-          .cl-blanco {
-            color: #ffff !important;
-          }
           .subtitle-dark {
             color: #575756;
             font-family: "mont-regular";
             font-size: 2rem;
-          }
-
-          .title-yesmom {
-            font-family: "mont-regular";
-            font-size: 1.2rem;
-          }
-          .subtitle-yesmom {
-            font-family: "mont-regular";
-            font-size: 1rem;
           }
 
           .btn-yellow {
@@ -224,7 +155,28 @@ const appLayout = ({ children }) => {
           .bg-gray {
             background-color: #575650 !important;
           }
-
+           {
+            /* @media (min-width: 576px) {
+            .box-web {
+              max-width: 900px !important;
+            }
+          }
+          @media (min-width: 900px) {
+            .box-web {
+              max-width: 992px !important;
+            }
+          }
+          @media (min-width: 992px) {
+            .box-web {
+              max-width: 1200px !important;
+            }
+          }
+          @media (min-width: 1200px) {
+            .box-web {
+              max-width: 1500px !important;
+            }
+          } */
+          }
           .box-yesmom {
             max-width: 1800px;
             width: 100%;
@@ -233,7 +185,7 @@ const appLayout = ({ children }) => {
           .link-a {
             color: #575756 !important;
             font-family: "mont-regular";
-            font-size: 1.5rem;
+            font-size: 1.1rem;
             text-decoration: none !important;
           }
           .link-a b {
@@ -481,50 +433,16 @@ const appLayout = ({ children }) => {
           .hover-efect-letter:hover:after {
             transform: scale(1.1);
           }
-          .card {
-            border-radius: 1rem !important;
-          }
-          .card-img,
-          .card-img-top {
-            border-top-left-radius: 1rem;
-            border-top-right-radius: 1rem;
-          }
-          .card-footer {
-            background: none;
-          }
-
-           {
-            /* style INPUT */
-          }
-
-          input {
-            text-align: center;
-          }
-          input:focus {
-            outline: none;
-            box-shadow: 0px 0px 3px #dc6a8d;
-          }
-          input::placeholder {
-            background-image: url("/image/header/lupa.svg");
-            background-size: contain;
-            background-position: 26rem;
-            background-repeat: no-repeat;
-            font-family: "mont-light";
-            color: #5a5a5a;
-            font-size: 0.9rem;
-            text-align: center;
-          }
-
           @media (min-width: 1800px) {
             .subtitle-dark {
               font-size: 1.2rem;
             }
-            input::placeholder {
-              background-position: 16rem;
-            }
           }
 
           @media (max-width: 768px) {
+            html {
+              font-size: 4.6vw !important;
+            }
             .view-desktop {
               display: none;
             }
@@ -551,9 +469,6 @@ const appLayout = ({ children }) => {
             }
             .link-a {
               font-size: 0.8rem;
-            }
-            input::placeholder {
-              background-position: 1rem;
             }
           }
 
@@ -710,7 +625,7 @@ const appLayout = ({ children }) => {
             -webkit-animation: kenburns-top 5s ease-out both;
             animation: kenburns-top 5s ease-out both;
           }
-
+          
           @-webkit-keyframes kenburns-top {
             0% {
               -webkit-transform: scale(1) translateY(0);
