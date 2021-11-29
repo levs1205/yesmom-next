68177c79dccb187dcfd14eef9e3e7b80e1c39333
zFranco23 <francoheflo@gmail.com>
2021-11-29 16:23:48 -0500

    Changes

--- context/actions/auth.js
+++ context/actions/auth.js
@@ -63,7 +63,7 @@ export const startLoginWithFacebook = async ( values ) => {
         // console.log(tokenId);
         // //LLamar endpoint para generar token
 
-        const { data } = await axios.get(`${process.env.NEXT_PUBLIC_REACT_APP_BACKEND_URL_SECURITY}/auth/facebook-profile?userId=${userID}?userType=U`,{ headers :  {
+        const { data } = await axios.get(`${process.env.NEXT_PUBLIC_REACT_APP_BACKEND_URL_SECURITY}/auth/facebook-profile?userId=${userID}&userType=U`,{ headers :  {
             'facebook-token' : accessToken
         }})
         console.log('data', data)
