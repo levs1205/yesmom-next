8e96c3531518124f1924ac4758992db395dbbcaf
NardaC <nardac6@gmail.com>
2022-02-17 15:35:13 -0500

    design

--- components/Footer.js
+++ components/Footer.js
@@ -19,6 +19,9 @@ const Footer = () => {
                   <b>CONTENIDO</b>
                 </h6>
                 <div className="box-contenido-footer">
+                <Link href="/nosotros">
+                    <a>Nosotros</a>
+                  </Link>
                   <Link href="/blog">
                     <a>Blog</a>
                   </Link>
@@ -26,14 +29,12 @@ const Footer = () => {
                     <a>Tienda</a>
                   </Link>
                   <Link href="/construccion">
-                    <a>Lista</a>
+                    <a>Regalos</a>
                   </Link>
                   <Link href="/construccion">
                     <a>Ayuda</a>
                   </Link>
-                  <Link href="/nosotros">
-                    <a>Sobre nosotros</a>
-                  </Link>
+                  
                   {/* <Link href="/nosotros">
                   <a>Términos y condiciones</a>
                 </Link> */}
