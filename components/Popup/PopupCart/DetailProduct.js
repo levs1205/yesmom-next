716cc562ffc147c480ada4947390fc13b64bdc59
zFranco23 <francoheflo@gmail.com>
2022-03-02 15:38:38 -0500

    Added more refactor and changes

--- components/Popup/PopupCart/DetailProduct.js
+++ components/Popup/PopupCart/DetailProduct.js
@@ -37,6 +37,22 @@ const DetailProduct = ({
     }
   }, []);
 
+  console.log(sizeSelected,colourSelected);
+
+  const makeDetails = useMemo(()=>{
+    let tempStr='';
+    if(sizeSelected || colourSelected){
+      if(sizeSelected && sizeSelected.length> 0){
+        tempStr+=`Talla: ${sizeSelected}`
+      }
+      if(colourSelected && colourSelected.length>0){
+        tempStr+=`${sizeSelected.length ? ' |' : ''}  Color: ${colourSelected}`
+      }
+    }
+
+    return tempStr;
+  },[sizeSelected,colourSelected])
+
   return (
     <>
       <div className="card--shopping-cart">
@@ -58,7 +74,7 @@ const DetailProduct = ({
         <div className="card--shopping-cart__text">
           <p className="card--shopping-cart__title">{nombre}</p>
           <p className="card--shopping-cart__size-color">
-            Talla: {sizeSelected} | Color: {colourSelected}
+            {makeDetails}
           </p>
           <p className="card--shopping-cart__price">
             S/
